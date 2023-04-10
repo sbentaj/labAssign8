9760639da578c755b8981248cf9fbcf356176a3b
Salma Bentaj <95006614+sbentaj@users.noreply.github.com>
2023-04-09 22:14:36 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -4,17 +4,107 @@
 
 int extraMemoryAllocated;
 
+void heaping( int arr[], int n, int i)
+{
+	int largest = i;
+	int l = 2 * i * 1;
+	int r = 2 * i * 2;
+	
+	if(l < n && arr[l] > arr[largest])
+		largest = l;
+	
+	if ( r < n && arr[r] > arr[largest])
+		largest = r;
+	
+	if (largrest != i)
+	{
+		intn temp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = temp;
+		heaping(arr, n. largest);
+	}
+	
+}
+
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	for (int i = n / 2 -1; i >= 0; i--)
+		heaping(arr, n, i);
+	
+	for (int i = n - 1; i > 0; i--)
+	{
+		int temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp; 
+		heaping(arr, i, 0);
+	}
 }
 
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	
+	int L[n1], R[n2];
+	
+	for(i = 0; i < n1; i++)
+		L[i] = arr[l +i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+	
+	i = 0;
+	j = 0;
+	k = l;
+	
+	while (i < n1 && j < n2)
+	{
+		if(L[i] <= R[j])
+		{
+			pData[k] = L[i];
+			i++;
+		}
+		
+		else 
+		{
+			pData[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while ( i < n1)
+	{
+		pData[k] = L[i];
+		i++;
+		k++;
+	}
+	
+	while ( j < n2) 
+	{
+		pData[k] = R[j];
+		j++;
+		k++;
+	}
+
+}
+
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r)
+	{
+		int m = l + (r - l) / 2;
+		
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+		
+		merge(pData, l, m, r);
+	}
 }
 
 // parses input file to an integer array
